9610623e4319df26a7c7af621faaad6b218c18a2
Evdokiya01 <95077590+Evdokiya01@users.noreply.github.com>
2024-05-25 22:58:45 +0300

    Delete routes directory

--- routes/games.js
+++
@@ -1,9 +0,0 @@
-const gamesRouter = require('express').Router();
-
-const findAllGames = require('../middlewares/games');
-const sendAllGames = require('../controllers/games');
-
-gamesRouter.get('/games', findAllGames, sendAllGames);
-gamesRouter.post("/games");
-
-module.exports = gamesRouter;
