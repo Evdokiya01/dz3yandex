23aa82ebc678e13da7c73a3980d051e9fb81ae02
Evdokiya01 <95077590+Evdokiya01@users.noreply.github.com>
2024-05-25 22:58:08 +0300

    Delete middlewares directory

--- middlewares/games.js
+++
@@ -1,8 +0,0 @@
-const games = require("../models/game");
-
-const findAllGames = async (req, res, next) => {
-  req.gamesArray = await games.find({}).populate("categories").populate("users");
-  next();
-};
-
-module.exports = findAllGames;
